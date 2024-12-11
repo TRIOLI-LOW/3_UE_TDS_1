257fe6c9f6f7cd1c25826bb3ee5fc080d1f25034
TRIOLI_LOW <trioli.low@gmail.com>
2024-12-11 19:47:21 +0300

    init

---
+++ Source/LeaveMeAlone/Private/LMADefaultCharacter.cpp
@@ -0,0 +1,94 @@
+ // Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LMADefaultCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/DecalComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+ALMADefaultCharacter::ALMADefaultCharacter()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	SpringArmComponent->SetUsingAbsoluteRotation(true);
+	SpringArmComponent->TargetArmLength = ArmLength;
+	SpringArmComponent->SetRelativeRotation(FRotator(YRotation, 0.0f, 0.0f));
+	SpringArmComponent->bDoCollisionTest = false;
+	SpringArmComponent->bEnableCameraLag = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	CameraComponent->SetFieldOfView(FOV);
+	CameraComponent->bUsePawnControlRotation = false; // данное условие запрещаем камере вращаться относительно SpringArmComponent.
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
+
+// Called when the game starts or when spawned
+void ALMADefaultCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsValid(CursorMaterial))
+	{
+		CurrentCursor = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), CursorMaterial, CursorSize, FVector(0));
+	}
+	
+}
+
+// Called every frame
+void ALMADefaultCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (PC)
+	{
+		FHitResult ResultHit;
+		PC->GetHitResultUnderCursor(ECC_GameTraceChannel1, true, ResultHit);
+		float FindRotatorResultYaw = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(),
+			ResultHit.Location).Yaw;
+		SetActorRotation(FQuat(FRotator(0.0f, FindRotatorResultYaw, 0.0f)));
+		if (CurrentCursor)
+		{
+			CurrentCursor->SetWorldLocation(ResultHit.Location);
+		}
+	}
+}
+
+// Called to bind functionality to input
+void ALMADefaultCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ALMADefaultCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ALMADefaultCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("Zoom", this, &ALMADefaultCharacter::ZoomCamera);
+
+}
+void ALMADefaultCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+void ALMADefaultCharacter::MoveRight(float Value)
+
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+void ALMADefaultCharacter::ZoomCamera(float Value) {
+  if (SpringArmComponent) {
+
+    float NewArmLength = FMath::Clamp(SpringArmComponent->TargetArmLength - (Value * ZoomSpeed),
+                                      MinArmLength, MaxArmLength);
+
+    SpringArmComponent->TargetArmLength = NewArmLength;
+	ArmLength = NewArmLength;
+  }
+}
---
+++ Source/LeaveMeAlone/Public/LMADefaultCharacter.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LMADefaultCharacter.generated.h"
+
+class UCameraComponent; // будет отвечать за компонент камеры.
+class USpringArmComponent; // используется для автоматического управления поведением камеры вситуациях, когда она становится закрытой.
+
+UCLASS()
+class LEAVEMEALONE_API ALMADefaultCharacter : public ACharacter
+{
+	// Sets default values for this character's properties
+	ALMADefaultCharacter();
+
+protected:
+		
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY()
+	UDecalComponent* CurrentCursor = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	UMaterialInterface* CursorMaterial = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	FVector CursorSize = FVector(20.0f, 40.0f, 40.0f);
+
+	virtual void BeginPlay() override;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+    void ZoomCamera(float Value);
+
+    float MinArmLength = 500.0f; 
+    float MaxArmLength = 2000.0f; 
+    float ZoomSpeed = 100.0f; 
+
+	float YRotation = -75.0f; // отвечает за поворот камеры по оси Y.
+	float ArmLength = 1400.0f; // отвечает за длину штатива.
+	float FOV = 55.0f; // отвечает за поле зрения камеры.
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
